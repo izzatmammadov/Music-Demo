46c4a3a6245c2bedd342f0ac24a52c5cecdea9e3
izzatmammadov <imamedov388@gmail.com>
2023-11-30 23:59:24 +0400

    format design patterns

---
+++ assets/js/playlist.js
@@ -0,0 +1,26 @@
+const songs = [
+    {
+        path: "./assets/music/song1.mp3",
+        displayName: "Numb",
+        arist: "Linkin Park",
+        cover: "./assets/images/image1.jpg"
+    },
+    {
+        path: "./assets/music/song2.mp3",
+        displayName: "Diet Mountain Dew",
+        arist: "Lana Del Rey",
+        cover: "./assets/images/image2.jpg"
+    },
+    {
+        path: "./assets/music/song3.mp3",
+        displayName: "Jeje",
+        arist: "Diamond Platnumz",
+        cover: "./assets/images/image3.jpg"
+    },
+    {
+        path: "./assets/music/song4.mp3",
+        displayName: "Demeanor",
+        arist: "Pop Smoke ft. Dua Lipa",
+        cover: "./assets/images/image4.jpg"
+    },
+]
